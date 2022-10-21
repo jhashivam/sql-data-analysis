ba4c63bbd522deeec8da3565ba322a5d09be4d02
Shivam Jha <shivamjhads@gmail.com>
2022-10-21 15:34:53 +0200

    Update Query

--- Employee_Info_Case_Study/employees.sql
+++
@@ -1,3 +0,0 @@
---  Sample employee database 
-
--- [employee.sql](https://drive.google.com/file/d/1147ZTRGOp-dFLTtAtPWD3KSqlp6_qGwm/view?usp=sharing)
---
+++ Employee_Info_Case_Study/employees.sql.md
@@ -0,0 +1,3 @@
+--  Sample employee database 
+
+* [employee.sql](https://drive.google.com/file/d/1147ZTRGOp-dFLTtAtPWD3KSqlp6_qGwm/view?usp=sharing)
